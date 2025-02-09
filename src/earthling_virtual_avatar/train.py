d1b634bb0351e74605c97fe873ca752e276a7fed
Jonatan Svendsen <Jhz209@alumni.ku.dk>
2025-02-09 20:21:38 +0100

    init cookiecutter project

---
+++ src/earthling_virtual_avatar/train.py
@@ -0,0 +1,10 @@
+from earthling_virtual_avatar.model import Model
+from earthling_virtual_avatar.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
